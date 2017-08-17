57956d31fb2fdaa63c1b9bf84026a5590129429e
halflkaka <raphael_shi@foxmail.com>
2017-08-17 15:21:50 +0800

    Add files via upload

---
+++ Bootstrapper.cs
@@ -0,0 +1,50 @@
+﻿
+using XmlCompare.Views;
+using System.Windows;
+using Prism.Mef;
+using System.ComponentModel.Composition.Hosting;
+using XmlCompare.Core;
+using XmlCompare.DAL;
+using XmlCompare.ToolSetting;
+using XmlCompare.UseControl;
+
+namespace XmlCompare
+{
+    class Bootstrapper : MefBootstrapper
+    {
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+            this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(GetType().Assembly));
+            this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(CoreModule).Assembly));
+            this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(DALModule).Assembly));
+            this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ToolSettingModule).Assembly));
+            this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(UseControlModule).Assembly));
+            //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(CheckModule).Assembly));
+            //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ScDbServiceModule).Assembly));
+            //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(EedDbServiceModule).Assembly));
+            //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ExcelServiceModule).Assembly));
+            //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(EmDbServiceModule).Assembly));
+
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.GetExportedValue<MainWindow>();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            //Application.Current.MainWindow = (MainWindow)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
